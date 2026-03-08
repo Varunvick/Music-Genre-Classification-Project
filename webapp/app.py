d5f2171ffa7b38cbe3485f48c5e3d5966fb5accb
Varikuppala varun <145549356+Varunvick@users.noreply.github.com>
2026-03-08 11:13:21 +0530

    add debug logs

--- webapp/app.py
+++ webapp/app.py
@@ -6,6 +6,15 @@ import numpy as np
 import joblib
 import os
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+model_path = os.path.join(BASE_DIR, "models", "model.pkl")
+
+print("BASE_DIR:", BASE_DIR)
+print("MODEL PATH:", model_path)
+
+model = joblib.load(model_path)
+print("Model loaded successfully")
 app = Flask(__name__)
 
 # load trained model
