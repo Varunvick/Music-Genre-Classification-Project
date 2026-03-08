590b9bb042b995f2d930accc0746840444f9f9d1
Varikuppala varun <145549356+Varunvick@users.noreply.github.com>
2026-03-08 12:03:59 +0530

    fix upload and model loading

--- webapp/app.py
+++ webapp/app.py
@@ -1,62 +1,68 @@
-from os import name
-
 from flask import Flask, render_template, request
 import librosa
 import numpy as np
 import joblib
 import os
 
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-model_path = os.path.join(BASE_DIR, "models", "model.pkl")
-
-print("BASE_DIR:", BASE_DIR)
-print("MODEL PATH:", model_path)
-
-model = joblib.load(model_path)
-print("Model loaded successfully")
 app = Flask(__name__)
 
-# load trained model
+# Base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-model_path = os.path.join(os.path.dirname(__file__), "..", "models", "model.pkl")
-model = joblib.load(model_path)
+# Paths
+UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
+MODEL_PATH = os.path.join(BASE_DIR, "models", "model.pkl")
 
-# home page
+# Create uploads folder if it doesn't exist
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
+print("BASE_DIR:", BASE_DIR)
+print("MODEL PATH:", MODEL_PATH)
+
+# Load trained model
+model = joblib.load(MODEL_PATH)
+print("Model loaded successfully")
+
+# Home page
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# prediction route
-
+# Prediction route
 @app.route("/predict", methods=["POST"])
 def predict():
+
     if "file" not in request.files:
         return "No file uploaded"
+
     try:
         file = request.files["file"]
-        file_path = os.path.join(os.path.dirname(__file__), "..", "uploads", "uploaded_song.wav")
+
+        file_path = os.path.join(UPLOAD_FOLDER, "uploaded_song.wav")
         file.save(file_path)
+
     except Exception as e:
         return f"Error saving file: {e}"
 
-    # load audio
+    # Load audio
     audio, sample_rate = librosa.load(file_path, duration=5)
-    # extract MFCC features
+
+    # Extract MFCC features
     mfcc = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=10)
     mfcc_scaled = np.mean(mfcc.T, axis=0)
 
-    # reshape for model
+    # Reshape for model
     mfcc_scaled = mfcc_scaled.reshape(1, -1)
 
-    # predict genre
+    # Predict genre
     prediction = model.predict(mfcc_scaled)
 
     return render_template("index.html", genre=prediction[0])
 
-import os
+
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+ 
     app.run(host="0.0.0.0", port=port)
